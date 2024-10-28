008f36ee7200ddc32c4295c44e364c196bfbc2c8
SammieDev <lythekiet333@gmail.com>
2024-10-28 20:47:25 +1030

    Add HotelRoom Table
    Included all common attributes of a hotel room

---
+++ Domain/Entities/HotelRoom.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class HotelRoom
+    {
+
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Address { get; set; }
+        public string? Description{ get; set; }
+        public string? PhoneNumber { get; set; }
+        public int Occupancy { get; set; }
+        public string? ImageUrl { get; set; }
+        [Required]
+        public double price { get; set; }
+        [Required]
+        public double size { get; set; }
+
+    }
+}
