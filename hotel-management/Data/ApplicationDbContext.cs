d67dd497055475d05e01799c8abf074fe1dd7ce0
SammieDev <lythekiet333@gmail.com>
2024-10-28 19:38:50 +1030

    Install all the required packages
    SQL Server and Tools from EntityFrameworkCore

---
+++ hotel-management/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using hotel_management.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace hotel_management.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
---
+++ hotel-management/Models/Genre.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace hotel_management.Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }    
+    }
+}
