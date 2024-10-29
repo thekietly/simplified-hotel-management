f599d2da93c52a4ce7a515d0a0578337f16a450e
SammieDev <lythekiet333@gmail.com>
2024-10-29 19:44:53 +1030

    Initialize data for HotelRoom
    This is done inside ApplicationDbContext - OnModelCreating

--- Infrastructure/Data/ApplicationDbContext.cs
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
 
 namespace Infrastructure.Data
 {
@@ -13,6 +15,24 @@ namespace Infrastructure.Data
         {
 
 
+        }
+        public DbSet<HotelRoom> HotelRooms { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // initializing data for the model - HotelRoom
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<HotelRoom>().HasData(new HotelRoom
+            {
+                Id = 1,
+                Name = "Standard Room",
+                Description = "Standard Room Description",
+                Address = "123 Main St, New York, NY 10001",
+                Occupancy = 2,
+                ImageUrl = "https://acihome.vn/uploads/15/tieu-chuan-biet-thu-5-sao-co-canh-quan-dep-gan-gui-voi-thien-nhien.jpg",
+                price = 90,
+                size = 200,
+                PhoneNumber = "123-456-7890"
+            });
         }
     }
 }
