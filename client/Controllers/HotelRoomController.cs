ce09ab0efbbde408ec403b8f84b4f64b65de26aa
SammieDev <lythekiet333@gmail.com>
2024-10-29 21:31:33 +1030

    Create HotelRoomController.cs
    Link to db and pass hotel room model

---
+++ client/Controllers/HotelRoomController.cs
@@ -0,0 +1,22 @@
+﻿using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace client.Controllers
+{
+    public class HotelRoomController : Controller
+    {
+        // database context
+        private readonly ApplicationDbContext _db;
+
+        public HotelRoomController(ApplicationDbContext context)
+        {
+            _db = context;
+        }
+
+        public IActionResult Index()
+        {
+            var hotelRooms = _db.HotelRooms.ToList();
+            return View(hotelRooms);
+        }
+    }
+}
