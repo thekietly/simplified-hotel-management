17405b10aea013bf01a47e2338c1d86232b25ae1
SammieDev <lythekiet333@gmail.com>
2024-10-31 21:09:25 +1030

    Complete delete hotel room feature
    Change <a> tag from delete button to <button> embedded inside a <form>.

--- client/Controllers/HotelRoomController.cs
+++ client/Controllers/HotelRoomController.cs
@@ -42,7 +42,12 @@ namespace client.Controllers
             _db.SaveChanges();
             return RedirectToAction("Index", "HotelRoom");
         }
-
+        /*
+         Purposes of Update method:
+        1. Retrieve the hotel room with the given id from the database.
+        2. If the hotel room is not found, return a 404 Not Found response.
+        3. If the hotel room is found, return a view with the hotel room as the model.
+         */
         public IActionResult Update(int? id) { 
 
             HotelRoom hotelRoom = _db.HotelRooms.FirstOrDefault(hr => hr.Id == id);
@@ -55,24 +60,41 @@ namespace client.Controllers
             return View(hotelRoom);
         }
 
-        //[HttpPost]
-        //public IActionResult Update(HotelRoom hotelRoom)
-        //{
-        //    if (hotelRoom.Name == hotelRoom.Description)
-        //    {
-        //        // key in AddModelError refers to the property in the model - in this case, error appears under the Name property
-        //        ModelState.AddModelError("Name", "Please be creative! The room's name cannot be the same as its description.");
-        //    }
-        //    // if model is not entered correctly return view
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return View();
-        //    }
-        //    // if user inputs are valid then add hotel room to database
-        //    _db.HotelRooms.Add(hotelRoom);
-        //    // update database
-        //    _db.SaveChanges();
-        //    return RedirectToAction("Index", "HotelRoom");
-        //}
+        [HttpPost]
+        public IActionResult Update(HotelRoom hotelRoom)
+        {
+            if (hotelRoom.Name == hotelRoom.Description)
+            {
+                // key in AddModelError refers to the property in the model - in this case, error appears under the Name property
+                ModelState.AddModelError("Name", "Please be creative! The room's name cannot be the same as its description.");
+            }
+            // if model is not entered correctly return view
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            // if user inputs are valid then update hotel room details
+            _db.HotelRooms.Update(hotelRoom);
+            // update database
+            _db.SaveChanges();
+            return RedirectToAction("Index", "HotelRoom");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int? id)
+        {
+
+            HotelRoom hotelRoom = _db.HotelRooms.FirstOrDefault(hr => hr.Id == id);
+            // any edge case where the hotel room is not found? go directly to this page without hotel room?
+            if (hotelRoom == null)
+            {
+                // TODO: Try to find a way to trigger this edge case
+                return RedirectToAction("Error", "Home");
+            }
+            _db.HotelRooms.Remove(hotelRoom);
+            _db.SaveChanges();
+            return RedirectToAction("Index", "HotelRoom");
+
+        }
     }
 }
