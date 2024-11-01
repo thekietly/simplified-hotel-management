418b0f668d79bfdda0fdd91ebdaa4a67e5c7c155
SammieDev <lythekiet333@gmail.com>
2024-11-01 19:42:26 +1030

    Create Hotelroom entity
    A hotel will have a list of rooms; different types of rooms. i.e Superior double room, Twin room, etc

---
+++ Domain/Entities/HotelRoom.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class HotelRoom
+    {
+        [Key]
+        public int RoomId { get; set; }
+        [ForeignKey("Hotel")]
+        public int HotelId { get; set; }
+        public Hotel Hotel { get; set; }
+    }
+}
