c20742f787190891392bb703e8b3d04b1666cc57
SammieDev <lythekiet333@gmail.com>
2024-11-05 18:43:38 +1030

    Create HotelRoomViewModel.cs
    This is for creating a drop down menu for hotels

---
+++ client/Models/HotelRoomViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace client.Models
+{
+    public class HotelRoomViewModel
+    {
+        public HotelRoom HotelRoom { get; set; }
+        public List<SelectListItem> Hotels { get; set; }
+    }
+}
