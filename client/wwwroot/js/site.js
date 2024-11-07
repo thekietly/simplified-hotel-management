0370316d311cfa2da64a760aaa14eeba795ac6f1
SammieDev <lythekiet333@gmail.com>
2024-11-07 19:31:16 +1030

    Bring the delete form to my custom js

--- client/wwwroot/js/site.js
+++ client/wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+// Submit the form with the given id
+function submitDeleteForm(id) {
+    document.getElementById('deleteForm-' + id).submit();
+}
