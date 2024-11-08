7b5f4feec38cef8a6fa216417eb9ff1e38829532
SammieDev <lythekiet333@gmail.com>
2024-11-08 19:06:35 +1030

    Completed user model with validation
    Ask user to enter account details and their personal details

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public enum Gender
+    {
+        Male,
+        Female,
+        NonBinary,
+        Other,
+        PreferNotToSay
+    }
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter your first name")]
+        [StringLength(100, ErrorMessage = "Please do not enter values over 100 characters")]
+        public required string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your last name")]
+        [StringLength(100, ErrorMessage = "Please do not enter values over 100 characters")]
+        public required string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
+        [RegularExpression("^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$", ErrorMessage = "Please enter a valid email address. An example could be account1@example.com.")]
+        public required string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your password")]
+        [StringLength(64, ErrorMessage = "Please do not enter values over 64 characters")]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
+        public required string Password { get; set; }
+
+        [Required(ErrorMessage = "Please select your gender")]
+        public Gender Gender { get; set; }
+
+        public DateTime ?CreatedAt { get; set; }
+        public DateTime ?UpdatedAt { get; set; }
+    }
+}
