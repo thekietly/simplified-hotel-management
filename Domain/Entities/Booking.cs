192d4473ebc5fb5149f36e6abb3d3d92a3ab5452
SammieDev <lythekiet333@gmail.com>
2024-11-08 19:54:15 +1030

    Create booking class
    This stores the price of the booking and all relevant information about it.

---
+++ Domain/Entities/Booking.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int HotelRoomId { get; set; }
+        public string BookingReference { get; set; }
+        public string Status { get; set; }
+        public double TotalPrice { get; set; }
+        public int NumberOfNights { get; set; }
+
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+    }
+}
