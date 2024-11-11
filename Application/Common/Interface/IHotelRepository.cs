30e48951b2d35e5c374c3f243c5a082484f3f36a
SammieDev <lythekiet333@gmail.com>
2024-11-11 19:48:16 +1030

    Added IHotelRepository in Application layer
    This is for separation of concerns. Avoid spaghetti code in the controller.

---
+++ Application/Common/Interface/IHotelRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Application.Common.Interface
+{
+    public interface IHotelRepository
+    {
+        // Return all hotels
+        IEnumerable<Hotel> GetAll(Expression<Func<Hotel, bool>>? filter = null, string? includeProperties = null);
+        IEnumerable<Hotel> Get(Expression<Func<Hotel, bool>> filter, string? includeProperties = null);
+
+        // Mimic the CRUD operations in the controller
+        // .Add a hotel
+        void Add(Hotel entity);
+        // .Update a hotel
+        void Update(Hotel entity);
+        // .Remove a hotel
+        void Remove(Hotel entity);
+        // .SaveChanges()
+        void Save();
+    }
+}
