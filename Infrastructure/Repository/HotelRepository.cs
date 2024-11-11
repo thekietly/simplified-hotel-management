cc9d41fac112739d1022e541ed7e5ff8f6179dc7
SammieDev <lythekiet333@gmail.com>
2024-11-11 19:49:27 +1030

    Added HotelRepository
    This class will be responsible for handling all the interactions between database and the server

---
+++ Infrastructure/Repository/HotelRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Common.Interface;
+using Domain.Entities;
+
+namespace Infrastructure.Repository
+{
+    public class HotelRepository : IHotelRepository
+    {
+        public void Add(Hotel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Hotel> Get(Expression<Func<Hotel, bool>> filter, string? includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Hotel> GetAll(Expression<Func<Hotel, bool>>? filter = null, string? includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Hotel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Hotel entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
