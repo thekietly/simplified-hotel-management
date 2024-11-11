b4bb381d47e5d747f515085343eaa0be2a514da0
SammieDev <lythekiet333@gmail.com>
2024-11-11 21:55:22 +1030

    Completed Get method in HotelRepository
    Provides logic for interface IHotelRepository Get().
    Applies filter and include accordingly like database query.

--- Infrastructure/Repository/HotelRepository.cs
+++ Infrastructure/Repository/HotelRepository.cs
@@ -6,39 +6,66 @@ using System.Text;
 using System.Threading.Tasks;
 using Application.Common.Interface;
 using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repository
 {
     public class HotelRepository : IHotelRepository
     {
+        private readonly ApplicationDbContext _db;
+        public HotelRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public void Add(Hotel entity)
         {
-            throw new NotImplementedException();
+            _db.Add(entity);
         }
 
-        public IEnumerable<Hotel> Get(Expression<Func<Hotel, bool>> filter, string? includeProperties = null)
+        public Hotel Get(Expression<Func<Hotel, bool>> filter, string? includeProperties = null)
         {
-            throw new NotImplementedException();
+            IQueryable<Hotel> query = _db.Set<Hotel>();
+            // if filter is not null, apply the filter i.e h => h.Id == 1
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            // if include properties is not null, include them i.e include(h => h.Hotel)
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var property in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(property);
+                }
+            }
+            return query.FirstOrDefault();
         }
 
         public IEnumerable<Hotel> GetAll(Expression<Func<Hotel, bool>>? filter = null, string? includeProperties = null)
         {
-            throw new NotImplementedException();
+            IQueryable<Hotel> query = _db.Set<Hotel>();
+            // if filter is not null, apply the filter i.e h => h.Id == 1
+            if (filter != null)
+            {
+                query
+
+
         }
 
         public void Remove(Hotel entity)
         {
-            throw new NotImplementedException();
+            _db.Remove(entity);
         }
 
         public void Save()
         {
-            throw new NotImplementedException();
+            _db.SaveChanges();
         }
 
         public void Update(Hotel entity)
         {
-            throw new NotImplementedException();
+            _db.Update(entity);
         }
     }
 }
