e1c765a277d5b15e82b6252493204e828556ca4e
SammieDev <lythekiet333@gmail.com>
2024-11-12 20:11:51 +1030

    GetAll() will have no tracking
    Disable tracking for GetAll(), because this is supposed to display a list of records of a selected entity. Usually this happens on the homepage.

--- Infrastructure/Repository/Repository.cs
+++ Infrastructure/Repository/Repository.cs
@@ -1,6 +1,8 @@
 ﻿using Application.Common.Interface;
+using Domain.Entities;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System.Linq.Expressions;
 
 
@@ -28,12 +30,41 @@ namespace Infrastructure.Repository
 
         public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
         {
-            throw new NotImplementedException();
+            IQueryable<T> query =dbSet;
+            // if filter is not null, apply the filter i.e h => h.Id == 1
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            // if include properties is not null, include them i.e include(h => h.Hotel)
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var property in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(property);
+                }
+            }
+            return query.FirstOrDefault();
         }
 
         public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null, bool tracked = false)
         {
-            throw new NotImplementedException();
+            // Read-only operations should not be tracked. No need to modify the records.
+            IQueryable<T> query = dbSet.AsNoTracking();
+            // if filter is not null, apply the filter i.e h => h.Id == 1
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            // if include properties is not null, include them i.e include(h => h.Hotel)
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var property in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(property);
+                }
+            }
+            return query.ToList();
         }
 
         public void Remove(T entity)
