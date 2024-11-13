5ee2554417f32afb171d187e4ee0c909d64e6ba0
SammieDev <lythekiet333@gmail.com>
2024-11-13 19:43:04 +1030

    Added unit of work
    This handles Hotel repo and save changes to the database asynchronously

---
+++ Application/Common/Interface/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Interface
+{
+    public interface IUnitOfWork
+    {
+        public IHotelRepository Hotel { get; }
+        Task Save();
+    }
+}
---
+++ Infrastructure/Repository/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿
+using Application.Common.Interface;
+using Infrastructure.Data;
+
+namespace Infrastructure.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        public IHotelRepository Hotel { get; private set; }
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Hotel = new HotelRepository(_db);
+        }
+        // Save changes to the database one by one.
+        public async Task Save()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
