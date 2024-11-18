b0b24663eedd3ad36c1170acb416960871fac35d
SammieDev <lythekiet333@gmail.com>
2024-11-18 19:18:54 +1030

    Added Amenity class
    A class to hold information about the room benefits such as, air con and housekeeper services.

---
+++ Domain/Entities/Amenity.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Entities
+{
+    public class Amenity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Icon { get; set; }
+
+
+
+    }
+}
