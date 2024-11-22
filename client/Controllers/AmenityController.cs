aae9f5943abc4d4b1ac87128e2f4a1c64a852f73
SammieDev <lythekiet333@gmail.com>
2024-11-22 20:12:57 +1030

    Completed Create and Delete actions for amenity

--- client/Controllers/AmenityController.cs
+++ client/Controllers/AmenityController.cs
@@ -1,4 +1,6 @@
 ﻿using Application.Common.Interface;
+using Domain.Entities;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,18 +15,66 @@ namespace client.Controllers
             _unitOfWork = unitOfWork;
         }
         // GET: AmenityController
-        public ActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var amenities = _unitOfWork.Amenity.GetAll();
-            return View();
+            var amenities = await _unitOfWork.Amenity.GetAll();
+            return View(amenities);
         }
 
         // GET: AmenityController/Create
-        public ActionResult Create()
+        public async Task<IActionResult> Create()
         {
             return View();
         }
 
+        // POST: AmenityController/Create
+        [HttpPost]
+        public IActionResult Create(Amenity amenity)
+        {
+            try
+            {
+                // if not valid
+                if (!ModelState.IsValid)
+                {
+                    return View(amenity);
+                }
+                // Check if the amenity name already exists in the database
+                if (_unitOfWork.Amenity.Any(a => a.Name == amenity.Name))
+                {
+                    // key in AddModelError refers to the property in the model - in this case, error appears under the Name property
+                    ModelState.AddModelError("Name", "An amenity with the same name already exists.");
+                    return View(amenity);
+                }
+                // if user inputs are valid then add hotel room to database
+                _unitOfWork.Amenity.Add(amenity);
+                // update database
+                _unitOfWork.Save();
+                return RedirectToAction("Index", "Amenity");
 
+            }
+            catch
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+        }
+        // GET: AmenityController/Delete
+        [HttpPost]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            // Return the amenity with the given id
+            Amenity amenity = await _unitOfWork.Amenity.Get(a => a.Id == id);
+            // any edge case where the hotel room is not found? go directly to this page without hotel room?
+            if (amenity == null)
+            {
+                TempData["Error"] = "The amenity you are trying to delete does not exist.";
+
+                return RedirectToAction("Error", "Home");
+            }
+            _unitOfWork.Amenity.Remove(amenity);
+            _unitOfWork.Save();
+            TempData["Success"] = amenity.Name + " has been deleted successfully.";
+            return RedirectToAction("Index", "Amenity");
+        }
     }
 }
