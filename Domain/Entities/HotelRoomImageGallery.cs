34e5d9f38e8e863921ba24daabd5c297ebac1b77
SammieDev <lythekiet333@gmail.com>
2024-11-28 22:06:19 +1030

    Created hotel room image gallery
    The purpose is the same as hotel gallery. It will store multiple images about the room.

---
+++ Domain/Entities/HotelRoomImageGallery.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public class HotelRoomImageGallery
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey("HotelRoom")]
+        public int RoomId { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        public virtual HotelRoom? HotelRoom { get; set; }
+    }
+}
