b68ade69cd82c363884fa2a16ec3de527f86e3c0
SammieDev <lythekiet333@gmail.com>
2024-11-28 22:05:48 +1030

    Created HotelImageGallery
    This is to store multiple images to display hotel image gallery onto the hotel detail page. So it doesn't look cheap for the design.

---
+++ Domain/Entities/HotelImageGallery.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public class HotelImageGallery
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey("Hotel")]
+        public int HotelId { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        public virtual Hotel? Hotel { get; set; }
+    }
+}
