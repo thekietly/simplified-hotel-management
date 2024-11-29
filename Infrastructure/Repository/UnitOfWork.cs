becf2674198e60d1d30261fc3b5306a144935334
SammieDev <lythekiet333@gmail.com>
2024-11-29 21:42:53 +1030

    Added hotel and room image gallery repo to unitofwork

--- Infrastructure/Repository/UnitOfWork.cs
+++ Infrastructure/Repository/UnitOfWork.cs
@@ -9,20 +9,23 @@ namespace Infrastructure.Repository
         private readonly ApplicationDbContext _db;
         public IHotelRepository Hotel { get; private set; }
         public IHotelRoomRepository HotelRoom { get; private set; }
-
-        // Can be re-factored to use AmenityUnitOfWork
         public IHotelAmenityRepository HotelAmenity { get; private set; }
         public IRoomAmenityRepository RoomAmenity { get; private set; }
         public IAmenityRepository Amenity { get; private set; }
+        public IHotelImageGalleryRepository HotelImageGallery { get; private set; }
+        public IHotelRoomImageGalleryRepository HotelRoomImageGallery { get; private set; }
 
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
+            // Initialize the repositories, they're all related closely to each other
             Hotel = new HotelRepository(_db);
             HotelRoom = new HotelRoomRepository(_db);
             Amenity = new AmenityRepository(_db);
             HotelAmenity = new HotelAmenityRepository(_db);
             RoomAmenity = new RoomAmenityRepository(_db);
+            HotelImageGallery = new HotelImageGalleryRepository(_db);
+            HotelRoomImageGallery = new HotelRoomImageGalleryRepository(_db);
         }
         public void Save()
         {
