1a347c2ad6a6476217ddc546e8462f5134c334c1
SammieDev <lythekiet333@gmail.com>
2024-11-29 21:29:01 +1030

    Created HotelRoomImageGallery repo and interface

--- Application/Common/Interface/IHotelRepository.cs
+++ Application/Common/Interface/IHotelRepository.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
-using Domain.Entities;
+﻿using Domain.Entities;
 
 namespace Application.Common.Interface
 {
---
+++ Infrastructure/Repository/HotelRoomImageGalleryRepository.cs
@@ -0,0 +1,18 @@
+﻿
+using Application.Common.Interface;
+using Domain.Entities;
+using Infrastructure.Data;
+
+namespace Infrastructure.Repository
+{
+    public class HotelRoomImageGalleryRepository : Repository<HotelRoomImageGallery>, IHotelRoomImageGalleryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public HotelRoomImageGalleryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+    {
+    }
+}
