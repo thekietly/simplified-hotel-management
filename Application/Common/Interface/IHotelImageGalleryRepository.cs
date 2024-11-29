31de3994c94057ea66e0d31b78a422899b41400c
SammieDev <lythekiet333@gmail.com>
2024-11-29 21:28:50 +1030

    Created HotelImageGallery repo and interface

---
+++ Application/Common/Interface/IHotelImageGalleryRepository.cs
@@ -0,0 +1,7 @@
+﻿using Domain.Entities;
+namespace Application.Common.Interface
+{
+    public interface IHotelImageGalleryRepository : IRepository<HotelImageGallery>
+    {
+    }
+}
---
+++ Infrastructure/Repository/HotelImageGalleryRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using Application.Common.Interface;
+using Infrastructure.Data;
+namespace Infrastructure.Repository
+{
+    public class HotelImageGalleryRepository : Repository<HotelImageGallery>, IHotelImageGalleryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public HotelImageGalleryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+    {
+    }
+}
