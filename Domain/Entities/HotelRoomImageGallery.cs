c08aaa170380ce3c1bbddb947eadcebf6a1cf4a8
SammieDev <lythekiet333@gmail.com>
2024-11-29 21:13:17 +1030

    Updated composite key of hotel room image gallery
    As the room table is a composite key, in order to reference this table, the whole primary key must be a composite key.

--- Domain/Entities/HotelRoomImageGallery.cs
+++ Domain/Entities/HotelRoomImageGallery.cs
@@ -5,15 +5,19 @@ namespace Domain.Entities
 {
     public class HotelRoomImageGallery
     {
-        [Key]
-        public int Id { get; set; }
 
-        [Required]
-        [ForeignKey("HotelRoom")]
-        public int RoomId { get; set; }
+        [Key, Column(Order = 0)]
+        public int RoomImageGalleryId { get; set; }
 
+        [Key, Column(Order = 1)]
+        public int HotelId { get; set; }
+        [Key, Column(Order = 2)]
+
+        public string RoomId { get; set; }
         public string? ImageUrl { get; set; }
 
         public virtual HotelRoom? HotelRoom { get; set; }
+        public virtual Hotel? Hotel { get; set; }
+
     }
 }
