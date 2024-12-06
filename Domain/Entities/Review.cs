7e97ea615898998c23507fb76c687257583d9a3a
SammieDev <lythekiet333@gmail.com>
2024-12-06 22:16:46 +1030

    Add 6 categories to rate in Review

--- Domain/Entities/Review.cs
+++ Domain/Entities/Review.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    internal class Review
+    public class Review
     {
+        public int Id { get; set; }
+        public int HotelId { get; set; }
+        [Range(0, 10)]
+        public int Location { get; set; }
+
+        [Range(0, 10)]
+        public int ValueForMoney { get; set; }
+
+        [Range(0, 10)]
+        public int Service { get; set; }
+
+        [Range(0, 10)]
+        public int Cleanliness { get; set; }
+
+        [Range(0, 10)]
+        public int Facilities { get; set; }
+
+        [Range(0, 10)]
+        public int RoomComfortAndQuality { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        public Hotel? Hotel { get; set; }
     }
 }
