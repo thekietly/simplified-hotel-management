4e67e8c1042656aca09faf1723c2c629bf7e8ed6
SammieDev <lythekiet333@gmail.com>
2024-12-10 23:14:55 +1030

    Completed REST APIs for Hotels
    Requires input validation to be fully completed- Let input validation service to do this.

--- API/Controllers/HotelController.cs
+++ API/Controllers/HotelController.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Common.Interface;
+using Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,14 +19,16 @@ namespace client.Controllers
         [HttpGet]
         public async Task<IActionResult> GetHotelDetails(int hotelId)
         {
-            var hotels = await _unitOfWork.Hotel.GetAll(include: q => q.Include(hr => hr.HotelRooms).Include(ha => ha.HotelAmenities).ThenInclude(a => a.Amenity));
+            var hotels = await _unitOfWork.Hotel.Get(filter: h => h.Id == hotelId);
+            // validate records before sending http code
+
             return Ok(hotels);
         }
         [Route("api/hotels/summary")]
         [HttpGet]
         public async Task<IActionResult> GetSummarisedHotelsInformation()
         {
-            var hotels = await _unitOfWork.Hotel.GetAll(include: q => q.Include(hr => hr.HotelRooms).Include(ha => ha.HotelAmenities).ThenInclude(a => a.Amenity));
+            var hotels = await _unitOfWork.Hotel.GetAll(include: q => q.Include(hr => hr.HotelRooms));
             return Ok(hotels);
         }
         [Route("api/hotels/full-details")]
@@ -35,5 +38,57 @@ namespace client.Controllers
             var hotels = await _unitOfWork.Hotel.GetAll(include: q => q.Include(hr => hr.HotelRooms).Include(ha => ha.HotelAmenities).ThenInclude(a => a.Amenity));
             return Ok(hotels);
         }
+
+        [Route("api/hotel/create")]
+        [HttpPost]
+        public Task<IActionResult> CreateHotel([FromBody] Hotel hotel)
+        {
+            if (!ModelState.IsValid)
+                return Task.FromResult<IActionResult>(BadRequest(ModelState));
+
+            hotel.CreatedDate = DateTime.UtcNow;
+            hotel.LastUpdated = DateTime.UtcNow;
+
+            _unitOfWork.Hotel.Add(hotel);
+            _unitOfWork.Save();
+            // returns 201 if successfully added
+            return Task.FromResult<IActionResult>(CreatedAtAction(nameof(GetHotelDetails), new { hotelId = hotel.Id }, hotel));
+        }
+        [Route("api/hotel/{hotelId}")]
+        [HttpPut]
+        public async Task<IActionResult> UpdateHotel(int hotelId, [FromBody] Hotel updatedHotel)
+        {
+            var hotelToBeUpdated = await _unitOfWork.Hotel.Get(filter: h => h.Id == hotelId);
+            if (hotelToBeUpdated == null)
+                return NotFound();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            hotelToBeUpdated.Name = updatedHotel.Name;
+            hotelToBeUpdated.Address = updatedHotel.Address;
+            hotelToBeUpdated.Description = updatedHotel.Description;
+            hotelToBeUpdated.ImageUrl = updatedHotel.ImageUrl;
+            hotelToBeUpdated.Size = updatedHotel.Size;
+            hotelToBeUpdated.LastUpdated = DateTime.UtcNow;
+
+            _unitOfWork.Hotel.Update(hotelToBeUpdated);
+            _unitOfWork.Save();
+            return Ok(hotelToBeUpdated);
+        }
+
+        [Route("api/hotel/{hotelId}")]
+        [HttpDelete]
+        public async Task<IActionResult> DeleteHotel(int hotelId)
+        {
+            var hotel = await _unitOfWork.Hotel.Get(filter: h => h.Id == hotelId);
+            if (hotel == null)
+                return NotFound();
+
+            _unitOfWork.Hotel.Remove(hotel);
+            _unitOfWork.Save();
+            return Ok();
+        }
+
     }
 }
