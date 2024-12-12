ae9e60b3487569548b33651d472cc948a0bc5c03
SammieDev <lythekiet333@gmail.com>
2024-12-12 22:59:49 +1030

    Created hotel room image gallery seed data
    Added 2 images for each of 2 rooms.

---
+++ Infrastructure/Data/HotelRoomImageGallerySeedData.cs
@@ -0,0 +1,39 @@
+﻿
+using Domain.Entities;
+
+namespace Infrastructure.Data
+{
+    public static class HotelRoomImageGallerySeedData
+    {
+        public static IEnumerable<HotelRoomImageGallery> GetHotelRoomImageGalleries() 
+        {
+            return new List<HotelRoomImageGallery>()
+            {
+                new HotelRoomImageGallery
+                {
+                    Id = 1,
+                    RoomId = 1,
+                    ImageUrl = "https://images.pexels.com/photos/3688261/pexels-photo-3688261.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                },
+                new HotelRoomImageGallery
+                {
+                    Id = 2,
+                    RoomId = 1,
+                    ImageUrl = "https://images.pexels.com/photos/3688261/pexels-photo-3688261.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                },
+                new HotelRoomImageGallery
+                {
+                    Id = 3,
+                    RoomId = 2,
+                    ImageUrl = "https://images.pexels.com/photos/3688261/pexels-photo-3688261.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                },
+                new HotelRoomImageGallery
+                {
+                    Id = 4,
+                    RoomId = 2,
+                    ImageUrl = "https://images.pexels.com/photos/3688261/pexels-photo-3688261.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                }
+            };
+        }
+    }
+}
