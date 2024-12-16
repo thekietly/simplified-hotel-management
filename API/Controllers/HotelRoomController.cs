9d46d29677dcd868d848b80654d2830ca0670427
SammieDev <lythekiet333@gmail.com>
2024-12-16 16:44:50 +1030

    Completed get-details and get-all for rooms

---
+++ API/Controllers/HotelRoomController.cs
@@ -0,0 +1,33 @@
+﻿using Application.Common.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    public class HotelRoomController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public HotelRoomController(IUnitOfWork unitOfWork) {
+            _unitOfWork = unitOfWork;
+        }
+        [Route("api/hotel-room/{RoomId}/get-details")]
+        [HttpGet]
+        public async Task<IActionResult> GetRoomDetails(int roomId) {
+            var roomDetails = await _unitOfWork.HotelRoom.Get(filter: r => r.Id == roomId);
+            if (roomDetails == null)
+            {
+                return NotFound();
+            }
+            return Ok(roomDetails);
+        }
+
+        [Route("api/hotel-room/get-all")]
+        [HttpGet]
+        public async Task<IActionResult> GetAllRooms() { 
+            var allRooms = await _unitOfWork.HotelRoom.GetAll();
+            return Ok(allRooms);
+        }
+
+
+    }
+}
