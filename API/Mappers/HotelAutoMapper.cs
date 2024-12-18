8cf7947b81282acd4e772a0b9296bf1239f957d4
SammieDev <lythekiet333@gmail.com>
2024-12-18 14:36:08 +1030

    Create hotel auto mapper
    Calculate average and total reviews to display to the thumbnail page.

---
+++ API/Mappers/HotelAutoMapper.cs
@@ -0,0 +1,25 @@
+﻿using API.Dtos.HotelDto;
+using Domain.Entities;
+using System.Linq;
+
+namespace API.Mappers
+{
+    public static class HotelAutoMapper
+    {
+        public static HotelDto ToHotelDto(this Hotel hotelModel) 
+        {
+            return new HotelDto
+            { 
+                Id = hotelModel.Id,
+                Name = hotelModel.Name,
+                Address = hotelModel.Address,
+                TotalReviews = hotelModel.Reviews?.Count() ?? 0,
+                AverageReview = hotelModel.Reviews?.Any() == true
+                ? hotelModel.Reviews
+                    .Select(r => (r.Location + r.ValueForMoney + r.Service + r.Cleanliness + r.Facilities + r.RoomComfortAndQuality) / 6.0)
+                    .Average()
+                : 0
+            };
+        }
+    }
+}
