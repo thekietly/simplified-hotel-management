588a8981c44c6e2aba34a001087bd1e00d863637
SammieDev <lythekiet333@gmail.com>
2024-12-18 15:52:10 +1030

    Remove reference loop checker from NewtonsoftJson
    [JsonIgnore] doesn't solve the circular reference loop therefore, this configuration is needed.

--- API/Program.cs
+++ API/Program.cs
@@ -6,8 +6,13 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers()
+    .AddNewtonsoftJson(options =>
+    {
+        // ignore reference loop
+        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+    });
 
-builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
