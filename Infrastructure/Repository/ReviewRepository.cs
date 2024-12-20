d72bafd5677502286099656505642549dad800d9
SammieDev <lythekiet333@gmail.com>
2024-12-20 19:02:17 +1030

    Add db link to review repository

---
+++ Infrastructure/Repository/ReviewRepository.cs
@@ -0,0 +1,16 @@
+﻿
+using Application.Common.Interface;
+using Domain.Entities;
+using Infrastructure.Data;
+
+namespace Infrastructure.Repository
+{
+    public class ReviewRepository : Repository<Review>, IReviewRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ReviewRepository(ApplicationDbContext db) : base(db)
+        { 
+            _db = db;
+        }
+    }
+}
