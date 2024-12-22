c63b25f4f73be43e5f9dcc25fdc394e4fbe7f4dd
SammieDev <lythekiet333@gmail.com>
2024-12-22 16:55:42 +1030

    Completed querying reviews to get overall info
    Transform review query results to overallReviewDto

---
+++ API/Controllers/ReviewController.cs
@@ -0,0 +1,30 @@
+﻿using Application.Common.Interface;
+using API.Mappers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ReviewController(IUnitOfWork unitOfWork) 
+        {
+            _unitOfWork = unitOfWork;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllReviews([FromQuery] int hotelId, [FromQuery] string type="overall") 
+        {
+            // Check if the request has hotel id
+            if (hotelId <= 0)
+            {
+                return BadRequest("Hotel ID is required.");
+            }
+            var reviews = await _unitOfWork.Review.GetAll(filter: r => r.HotelId == hotelId);
+            var overallReviewDto = reviews.ToOverallReviewDto();
+            return Ok(overallReviewDto);
+        }
+    }
+}
