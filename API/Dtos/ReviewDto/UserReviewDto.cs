ae9749a8723230ba6295b25559729f9062f75d82
SammieDev <lythekiet333@gmail.com>
2024-12-22 19:51:41 +1030

    Remove number of nights & guests
    These 2 fields can be simplified.

--- API/Dtos/ReviewDto/UserReviewDto.cs
+++ API/Dtos/ReviewDto/UserReviewDto.cs
@@ -5,8 +5,6 @@
         public int Id { get; set; }
         public required int UserId{ get; set; }
         public required string UserName { get; set; }
-        public int NumberOfNights { get; set; }
-        public int NumberOfGuests { get; set; }
         public double AverageReview { get; set; }
         public string? Like { get; set; }
         public string? Dislike { get; set; }
