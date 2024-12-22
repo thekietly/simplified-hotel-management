c034a6927e293552070a00debfd2652536766169
SammieDev <lythekiet333@gmail.com>
2024-12-22 16:42:23 +1030

    Calculate average reviews for the review dto

---
+++ API/Mappers/ReviewAutoMapper.cs
@@ -0,0 +1,50 @@
+﻿using API.Dtos.ReviewDto;
+using Domain.Entities;
+using System.Collections.Generic;
+
+namespace API.Mappers
+{
+    public static class ReviewAutoMapper
+    {
+        public static OverallReviewDto ToOverallReviewDto(this IEnumerable<Review> reviews) 
+        {
+            // If no reviews for this hotel
+            if (reviews == null || !reviews.Any())
+            {
+                return new OverallReviewDto
+                {
+                    AverageLocationReview = 0,
+                    AverageValueForMoneyReview = 0,
+                    AverageCleanlinessReview = 0,
+                    AverageServiceReview = 0,
+                    AverageFacilitiesnReview = 0,
+                    AverageRoomComfortAndQualityReview = 0,
+                    OverallAverageAllCategories = 0,
+                    TotalReviews = 0
+                };
+            }
+            // Calculate the average reviews of each category of this hotel
+            int totalReviews = reviews.Count();
+            var overallReviewDto = new OverallReviewDto
+            {
+                AverageLocationReview = reviews.Average(r => r.Location),
+                AverageValueForMoneyReview = reviews.Average(r => r.ValueForMoney),
+                AverageCleanlinessReview = reviews.Average(r => r.Cleanliness),
+                AverageServiceReview = reviews.Average(r => r.Service),
+                AverageFacilitiesnReview = reviews.Average(r => r.Facilities),
+                AverageRoomComfortAndQualityReview = reviews.Average(r => r.RoomComfortAndQuality),
+                TotalReviews = totalReviews
+            };
+            // Calculate the overall average reviews
+            overallReviewDto.OverallAverageAllCategories =
+                (overallReviewDto.AverageLocationReview +
+                overallReviewDto.AverageValueForMoneyReview +
+                overallReviewDto.AverageCleanlinessReview +
+                overallReviewDto.AverageServiceReview +
+                overallReviewDto.AverageFacilitiesnReview +
+                overallReviewDto.AverageRoomComfortAndQualityReview) / 6.0;
+
+            return overallReviewDto;
+        }
+    }
+}
