cf95387799ba6e67dad8af3a66ffa4cbb1844f23
SammieDev <lythekiet333@gmail.com>
2024-12-26 04:25:45 +1030

    Added dto and mapper for room amenity

---
+++ API/Dtos/RoomAmenityDto/RoomAmenityDto.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Dtos.RoomAmenityDto
+{
+    public class RoomAmenityDto
+    {
+        public int Id { get; set; }
+        public List<int> AmenityList { get; set; }
+    }
+}
---
+++ API/Mappers/RoomAmenityAutoMapper.cs
@@ -0,0 +1,26 @@
+﻿using API.Dtos.RoomAmenityDto;
+using Domain.Entities;
+
+namespace API.Mappers
+{
+    // Similar with HotelAmenityAutoMapper
+    public static class RoomAmenityAutoMapper
+    {
+        public static RoomAmenityDto ToRoomAmenityDto(this IEnumerable<RoomAmenity> roomAmenities) 
+        {
+            return new RoomAmenityDto
+            {
+                Id = roomAmenities.First().RoomId,
+                AmenityList = roomAmenities.Select(a => a.AmenityId).ToList()
+            };
+        }
+        public static List<RoomAmenity> ToRoomAmenityList(this RoomAmenityDto roomAmenityDto) 
+        {
+            return roomAmenityDto.AmenityList.Select(amenityId => new RoomAmenity 
+            {
+                RoomId = roomAmenityDto.Id,
+                AmenityId = amenityId
+            }).ToList();
+        }
+    }
+}
