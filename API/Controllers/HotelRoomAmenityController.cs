0a5369f597f70e7933201871a598852c098de96c
SammieDev <lythekiet333@gmail.com>
2024-12-26 03:43:00 +1030

    Readjust api routing names
    api/hotel-room/amenity

---
+++ API/Controllers/HotelRoomAmenityController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/hotel-room/amenity")]
+    [ApiController]
+    public class HotelRoomAmenityController : ControllerBase
+    {
+    }
+}
