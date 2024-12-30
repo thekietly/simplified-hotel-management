cbde54a430e8d595d767505f1a7bbe9577c94892
SammieDev <lythekiet333@gmail.com>
2024-12-30 17:22:25 +1030

    Add hotel facilities management service
    This provides queries about the hotel facilities.

---
+++ Application/SqlDatabaseContextService/IHotelManagementContextService.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+
+namespace Services.SqlDatabaseContextService
+{
+    // An interface that defines the basic CRUD operations
+    public interface IHotelManagementContextService : IApplicationContextService
+    {
+        #region Hotel
+        Task<Hotel> CreateHotelAsync(Hotel hotel);
+        Task<Hotel> GetHotelByIdAsync(int hotelId);
+        Task<Hotel> UpdateHotelAsync(Hotel hotel);
+        Task<Hotel> DeleteHotelAsync(int hotelId);
+        Task<Hotel> GetAllHotelsAsync();
+        Task<HotelAmenity> GetAllHotelAmenitiesAsync(int hotelId);
+        Task<ICollection<HotelAmenity>> AddAmenityToHotelAsync(ICollection<HotelAmenity> hotelAmenities);
+        Task<ICollection<HotelAmenity>> DeleteAmenitiesFromHotelAsync(ICollection<HotelAmenity> hotelAmenities);
+        Task<HotelImageGallery> GetAllHotelImageGalleryByIdAsync(int hotelId);
+        Task<ICollection<HotelImageGallery>> AddHotelImagesByIdAsync(ICollection<HotelImageGallery> hotelImageGalleries);
+        Task<ICollection<HotelImageGallery>> DeleteHotelImagesByIdAsync(ICollection<HotelImageGallery> hotelImageGalleries);
+        #endregion
+
+    }
+}
