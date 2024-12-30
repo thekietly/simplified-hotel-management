a7ca259bc39e3afc234f055f492a9b8352a05c9a
SammieDev <lythekiet333@gmail.com>
2024-12-30 17:22:43 +1030

    Create IRoomManagementContextService.cs
    This provides access to room facilities database

---
+++ Application/SqlDatabaseContextService/IRoomManagementContextService.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+namespace Services.SqlDatabaseContextService
+{
+    public interface IRoomManagementContextService : IApplicationContextService
+    {
+        #region Hotel Room
+        Task<HotelRoom> GetRoomByIdAsync(int roomId);
+        Task<HotelRoom> GetAllRoomsAsync();
+        Task<HotelRoom> CreateRoomAsync(HotelRoom hotelRoom);
+        Task<HotelRoom> UpdateRoomAsync(HotelRoom hotelRoom);
+        Task<HotelRoom> DeleteRoomAsync(int roomId);
+        Task<RoomAmenity> GetAllRoomAmenitiesAsync(int roomId);
+        Task<ICollection<RoomAmenity>> AddAmenitiesFromRoomAsync(ICollection<RoomAmenity> roomAmenities);
+        Task<ICollection<RoomAmenity>> DeleteAmenitiesFromRoomAsync(ICollection<RoomAmenity> roomAmenities);
+        Task<HotelRoomImageGallery> GetAllRoomImagesByIdAsync(int roomId);
+        Task<ICollection<HotelRoomImageGallery>> AddRoomImagesByIdAsync(ICollection<HotelRoomImageGallery> hotelRoomImageGalleries);
+        Task<ICollection<HotelRoomImageGallery>> DeleteRoomImagesByIdAsync(ICollection<HotelRoomImageGallery> hotelRoomImageGalleries);
+        #endregion
+    }
+}
