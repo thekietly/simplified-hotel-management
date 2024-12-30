03a6234a5f1e5e9f157153d30e89e38c0288afed
SammieDev <lythekiet333@gmail.com>
2024-12-30 17:36:08 +1030

    Add more methods to sql database repository
    These methods are required to be implemented

--- Infrastructure/Repository/SqlDatabaseRepository.cs
+++ Infrastructure/Repository/SqlDatabaseRepository.cs
@@ -1,16 +1,66 @@
-﻿using Infrastructure.Data;
-
+﻿using Domain.Entities;
+using Infrastructure.Data;
+using Services.SqlDatabaseContextService;
 
 namespace Infrastructure.Repository
 {
-    public class SqlDatabaseRepository
+    public class SqlDatabaseApplicationFacilityRepository : IApplicationFacilityContextService, IDisposable
     {
         private readonly ApplicationDbContext _db;
-        public SqlDatabaseRepository(ApplicationDbContext db)
+        public SqlDatabaseApplicationFacilityRepository(ApplicationDbContext db)
         {
             _db = db;
 
         }
 
+        public Task<Amenity> AddAmenityAsync(Amenity amenity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Review> CreateReviewAsync(Review review)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Amenity> DeleteAmenityAsync(Amenity amenity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Review> DeleteReviewAsync(Review review)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Amenity> GetAllAmenitiesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Review> GetAllReviewsByHotelIdAsync(int hotelId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Amenity> GetAmenityByIdAsync(int amenityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Review> GetReviewByReviewIdAsync(int reviewId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Review> UpdateReviewAsync(Review review)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
