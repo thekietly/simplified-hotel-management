c61341473c849b18a1da12fc68dcb59091e3bcd8
SammieDev <lythekiet333@gmail.com>
2024-12-30 17:36:28 +1030

    Create SqlDatabaseRoomRepository.cs
    There are methods that need to be implemented based on the interface

---
+++ Infrastructure/Repository/SqlDatabaseRoomRepository.cs
@@ -0,0 +1,69 @@
+﻿using Domain.Entities;
+using Infrastructure.Data;
+using Services.SqlDatabaseContextService;
+
+namespace Infrastructure.Repository
+{
+    public class SqlDatabaseRoomRepository : IRoomManagementContextService
+    {
+        private readonly ApplicationDbContext db;
+        public SqlDatabaseRoomRepository(ApplicationDbContext db) 
+        { 
+            this.db = db;
+        }
+        public Task<ICollection<RoomAmenity>> AddAmenitiesFromRoomAsync(ICollection<RoomAmenity> roomAmenities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<HotelRoomImageGallery>> AddRoomImagesByIdAsync(ICollection<HotelRoomImageGallery> hotelRoomImageGalleries)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HotelRoom> CreateRoomAsync(HotelRoom hotelRoom)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<RoomAmenity>> DeleteAmenitiesFromRoomAsync(ICollection<RoomAmenity> roomAmenities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HotelRoom> DeleteRoomAsync(int roomId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<HotelRoomImageGallery>> DeleteRoomImagesByIdAsync(ICollection<HotelRoomImageGallery> hotelRoomImageGalleries)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RoomAmenity> GetAllRoomAmenitiesAsync(int roomId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HotelRoomImageGallery> GetAllRoomImagesByIdAsync(int roomId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HotelRoom> GetAllRoomsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HotelRoom> GetRoomByIdAsync(int roomId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HotelRoom> UpdateRoomAsync(HotelRoom hotelRoom)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
