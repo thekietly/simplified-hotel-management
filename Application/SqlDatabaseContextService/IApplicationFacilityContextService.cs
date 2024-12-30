74dce8300fbc18899f0310b491ae2ef0d7f32a45
SammieDev <lythekiet333@gmail.com>
2024-12-30 17:34:27 +1030

    Create IApplicationFacilityContextService.cs
    This adds general queries about the database like reviews, amenities, reports ,etc

---
+++ Application/SqlDatabaseContextService/IApplicationFacilityContextService.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+
+namespace Services.SqlDatabaseContextService
+{
+    public interface IApplicationFacilityContextService
+    {
+        #region Amenity
+        Task<Amenity> GetAllAmenitiesAsync();
+        Task<Amenity> GetAmenityByIdAsync(int amenityId);
+        Task<Amenity> AddAmenityAsync(Amenity amenity);
+        Task<Amenity> DeleteAmenityAsync(Amenity amenity);
+        #endregion
+        #region Review
+        Task<Review> GetAllReviewsByHotelIdAsync(int hotelId);
+        Task<Review> GetReviewByReviewIdAsync(int reviewId);   
+        Task<Review> CreateReviewAsync(Review review);
+        Task<Review> DeleteReviewAsync(Review review);
+        Task<Review> UpdateReviewAsync(Review review);
+        #endregion
+    }
+}
