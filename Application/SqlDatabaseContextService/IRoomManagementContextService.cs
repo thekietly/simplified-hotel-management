79f158c7d2c287ba4059004f89925e006e1b8caa
SammieDev <lythekiet333@gmail.com>
2024-12-31 00:58:35 +1030

    Update room context service
    Added paged response and fix return types

--- Application/SqlDatabaseContextService/IRoomManagementContextService.cs
+++ Application/SqlDatabaseContextService/IRoomManagementContextService.cs
@@ -4,17 +4,17 @@ namespace Services.SqlDatabaseContextService
     public interface IRoomManagementContextService
     {
         #region Hotel Room
-        Task<HotelRoom> GetRoomByIdAsync(int roomId);
-        Task<HotelRoom> GetAllRoomsAsync();
-        Task<HotelRoom> CreateRoomAsync(HotelRoom hotelRoom);
-        Task<HotelRoom> UpdateRoomAsync(HotelRoom hotelRoom);
-        Task<HotelRoom> DeleteRoomAsync(int roomId);
-        Task<RoomAmenity> GetAllRoomAmenitiesAsync(int roomId);
-        Task<ICollection<RoomAmenity>> AddAmenitiesFromRoomAsync(ICollection<RoomAmenity> roomAmenities);
-        Task<ICollection<RoomAmenity>> DeleteAmenitiesFromRoomAsync(ICollection<RoomAmenity> roomAmenities);
-        Task<HotelRoomImageGallery> GetAllRoomImagesByIdAsync(int roomId);
-        Task<ICollection<HotelRoomImageGallery>> AddRoomImagesByIdAsync(ICollection<HotelRoomImageGallery> hotelRoomImageGalleries);
-        Task<ICollection<HotelRoomImageGallery>> DeleteRoomImagesByIdAsync(ICollection<HotelRoomImageGallery> hotelRoomImageGalleries);
+        Task<HotelRoom?> GetRoomByIdAsync(int roomId);
+        Task<PagedResult<HotelRoom>> GetAllRoomsAsync(int skip, int take);
+        Task<CreateResult> CreateRoomAsync(HotelRoom hotelRoom);
+        Task<UpdateResult> UpdateRoomAsync(HotelRoom hotelRoom);
+        Task<DeleteResult> DeleteRoomAsync(int roomId);
+        Task<ICollection<RoomAmenity>> GetAllRoomAmenitiesAsync(int roomId);
+        Task<ICollection<CreateResult>> AddAmenitiesFromRoomAsync(ICollection<RoomAmenity> roomAmenities);
+        Task<DeleteResult> DeleteAmenitiesFromRoomAsync(ICollection<RoomAmenity> roomAmenities);
+        Task<ICollection<HotelRoomImageGallery>> GetAllRoomImagesByIdAsync(int roomId);
+        Task<ICollection<CreateResult>> AddRoomImagesByIdAsync(ICollection<HotelRoomImageGallery> hotelRoomImageGalleries);
+        Task<DeleteResult> DeleteRoomImagesByIdAsync(ICollection<int> imageIds);
         #endregion
     }
 }
