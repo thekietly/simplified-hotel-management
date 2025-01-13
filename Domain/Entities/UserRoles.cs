3757fa08b8aac0a9f4f685567c1e25f33373e51c
SammieDev <lythekiet333@gmail.com>
2025-01-13 21:33:25 +1030

    Create user login model
    Requests user to enter username and password

---
+++ Domain/Entities/UserRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities
+{
+    public static class UserRoles
+    {
+        public const string HotelAdmin = "HotelAdmin";
+        public const string User = "User";
+        public const string WebsiteAdmin = "WebsiteAdmin";
+    }
+}
